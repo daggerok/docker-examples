c6ff9b1adb472643e755a37a1c37e7e58dac6412
Maksim Kostromin <daggerok@gmail.com>
2017-10-24 18:04:28 +0300

    update bash script

--- kong-docker-swarm-using-ucp/swarm-start.bash
+++ kong-docker-swarm-using-ucp/swarm-start.bash
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
-set -x
+
 export APP_NAME="kong-application"
 export CONTAINER_NAME="ucp"
 
-bash ./bash/init-swarm-cluster-using-ucp.bash
-bash ./bash/create-application-network.bash
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-bash ./bash/create-and-run-stateful-db-service.bash
-bash ./bash/run-kong-database-migrations.bash
-bash ./bash/run-kong-service.bash
-bash ./bash/run-dashboard-service.bash
+bash ${DIR}/bash/init-swarm-cluster-using-ucp.bash
+bash ${DIR}/bash/create-application-network.bash
+
+bash ${DIR}/bash/create-and-run-stateful-db-service.bash
+bash ${DIR}/bash/run-kong-database-migrations.bash
+bash ${DIR}/bash/run-kong-service.bash
+bash ${DIR}/bash/run-dashboard-service.bash
 
 echo "application is ready"
 docker service ls
